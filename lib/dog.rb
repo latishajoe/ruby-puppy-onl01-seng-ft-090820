b8839cc2d166ca917dfeab427c4536e0fa84526a
Latisha Joe <latisha_joe@yahoo.com>
2020-09-21 12:54:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Add your code here
+class Dog
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+     @name = name
+     @@all << self
+  end
+
+  def self.all
+     @@all
+  end
+
+  def self.clear_all
+
+
+
+
+
+
+end 
